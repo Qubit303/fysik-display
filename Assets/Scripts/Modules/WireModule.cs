7087d849d4a93a9251021ba19dcadeda177da77e
Qubit <malthebroman@gmail.com>
2024-05-14 13:33:17 +0200

    erm

--- Assets/Scripts/Modules/WireModule.cs
+++ Assets/Scripts/Modules/WireModule.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WireModule", menuName = "Modules/Wire Module")]
+[CreateAssetMenu(fileName = "WireModule", menuName = "Modules/Wire")]
 public class WireModule : BaseModule
 {
     void OnValidate()
